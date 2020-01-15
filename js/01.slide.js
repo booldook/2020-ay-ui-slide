8e7e80ee0f38d44941b5086fbcbbd8bd38d757d6
booldook <booldook@gmail.com>
2020-01-15 11:18:30 +0900

    프로그램 짜기 시작

--- js/01.slide.js
+++ js/01.slide.js
@@ -1,3 +1,4 @@
+/* Data */
 var datas = [
 	{id: 1, src: "./img/p1.png", alt: "배너1", link: "#"},
 	{id: 2, src: "./img/p2.png", alt: "배너2", link: "#"},
@@ -6,6 +7,7 @@ var datas = [
 	{id: 5, src: "./img/p5.png", alt: "배너5", link: "#"}
 ];
 
+/* Slide 만드는 함수 */
 function slideMaker($obj, dt, clsName, isClone) {
 	var html = '';
 	for(var i in dt) {
@@ -19,6 +21,7 @@ function slideMaker($obj, dt, clsName, isClone) {
 	if(isClone) $obj.append(	$("."+clsName).eq(0).clone()	);
 }
 
+/* Pager 만드는 함수 */
 function pagerMaker($obj, dt, clsName) {
 	var html = '';
 	for(var i in dt) {
@@ -33,14 +36,27 @@ function ani() {
 
 }
 
+/* 프로그램 시작 */
+
+/* jQuery객체선언 및 전역변수 선언 */
 var $stage = $(".stage");
 var $slides =  $stage.find(".slides");
 var $slide =  $stage.find(".slide");
 var $pager = $stage.find(".pagers");
+var interval;
+var speed = 500;
+var delay = 4000;
+var now = 0;
 
+/* 객체(슬라이드 및 페이저) 생성 */
 slideMaker($slides, datas, "slide", true);
 pagerMaker($pager, datas, "pager");
 
 
+/* 이벤트선언 */
+interval = setInterval(intervalFn, delay);
 
-
+/* 함수 */
+function intervalFn() {
+	console.log("실행");
+}
