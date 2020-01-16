47845250c9675530035d4b5aa52673b141b984a9
booldook <booldook@gmail.com>
2020-01-16 09:47:11 +0900

    완성!!!

--- js/01.slide.js
+++ js/01.slide.js
@@ -204,4 +204,81 @@ function pagerMaker($obj, dt, clsName) {
 	}
 })();
 
+/* 페이드형 */
+(function(){
+	/* jQuery객체선언 및 전역변수 선언 */
+	var $stage = $("#stage3");
+	var $slides =  $stage.find(".slides");
+	var $pagers = $stage.find(".pagers");
+	var $prev = $stage.find(".prev");
+	var $next = $stage.find(".next");
+	var interval;
+	var speed = 500;
+	var delay = 4000;
+	var now = 0;
+	var last, $slide, $pager;
+	
+	/* 객체(슬라이드 및 페이저) 생성 */
+	slideMaker($slides, datas, "slide", true);
+	pagerMaker($pagers, datas, "pager");
+	
+	$pager = $stage.find(".pager");
+	$slide = $slides.find(".slide");
+	last = $slide.length;
+	
+	/* 이벤트선언 */
+	var interval = setInterval(intervalSlide, delay);
+	$stage.mouseenter(pauseSlide);
+	$stage.mouseleave(playSlide);
+	$prev.click(prevSlide);
+	$next.click(nextSlide);
+	$pager.click(movingSlide);
+	$stage.imagesLoaded(function(){
+		$(window).resize(resizeSlide).trigger("resize");
+	});
+	pagerChg();
+	
+	/* 함수 */
+	function ani() {
+		$slide.stop().animate({"opacity": 0}, speed);
+		$slide.eq(now).stop().animate({"opacity": 1}, speed);
+		pagerChg();
+	}
+	function resizeSlide() {
+		var hei = $slide.outerHeight();
+		$stage.outerHeight(hei);
+	}
+	function pagerChg() {
+		$pager.removeClass("active");
+		$pager.eq(now).addClass("active");
+		if(now == last - 1) $pager.eq(0).addClass("active");
+	}
+	function movingSlide() {
+		now = $(this).index();
+		ani();
+	}
+	function nextSlide() {
+		if(now < last - 2) now++;
+		ani();
+	}
+	function prevSlide() {
+		if(now > 0) now--;
+		ani();
+	}
+	function playSlide() {
+		interval = setInterval(intervalSlide, delay);
+	}
+	function pauseSlide() {
+		clearInterval(interval);
+	}
+	function intervalSlide() {
+		if(now == last - 1) {
+			now = 0;
+			$slides.css({"left": 0});
+		}
+		now++;
+		ani();
+	}
+})();
+
 
